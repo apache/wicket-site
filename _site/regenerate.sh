84e1c9fb57aa575af44a04faf44f8ae425f05e50
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-05-09 10:05:59 +0300

    Rebuild the site to test gitpubsub

--- _site/regenerate.sh
+++ _site/regenerate.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-#workaround script for regenerating the site because jekyll completely wipes _site which causes problems for svn
-echo Backing up .svn folders in _site
-mkdir _tmp
-tar cfT _tmp/archive.tar /dev/null
-find _site -name ".svn"|xargs -I{} tar -rf _tmp/archive.tar {}
-#jekyll
 jekyll build
-echo Restoring .svn folders in _site
-tar -xf _tmp/archive.tar
-rm -rf _tmp
-
